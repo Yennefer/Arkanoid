8dbd12c3d370488c017b5a9d3383c530345bc8ec
Alena Kabadinova <zakomoldinaav@gmail.com>
2017-10-01 15:11:32 +0500

    :sparkles: Brick collision added

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		if (coll.gameObject.tag.Equals ("Ball")) {
+			gameObject.SetActive (false);
+		}
+	}
+}
